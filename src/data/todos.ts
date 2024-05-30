07b4a4d6a3819b81cd607b8896362e689d283ea7
Coder-3007 <coder3007@gmail.com>
2024-05-30 15:21:13 +0100

    data

---
+++ src/data/todos.ts
@@ -0,0 +1,19 @@
+import { Todo } from "../Type/todo";
+
+export const dummyData: Todo[] = [
+  {
+    id: 1,
+    title: "Like the Code 👍",
+    completed: false,
+  },
+  {
+    id: 2,
+    title: "I Love to Code 🧑‍💻",
+    completed: false,
+  },
+  {
+    id: 3,
+    title: "Coding makes me happy 😀",
+    completed: false,
+  },
+];
